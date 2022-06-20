eaa08ea443002bd1254cdd1de016455b5ea06b91
ddanilooalves <daniloalves0512@gmail.com>
2022-06-20 20:30:58 -0300

    docs: swagger install

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({
+    summary: 'Visuzalizar status da aplicação'
+  })
+  getStatus(): string {
+    return this.appService.getStatus('http://localhost:3000');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('X Box')
+    .setDescription('Aplicação para gestão da plataforma de jogos')
+    .setVersion('1.0.0')
+    .addTag('status')
+
+    
   await app.listen(3000);
 }
 bootstrap();
